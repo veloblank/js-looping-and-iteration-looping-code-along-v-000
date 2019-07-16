daf744984edba171bc0084085224a9ad5260c957
John Blank <veloblank@gmail.com>
2019-07-16 13:52:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const newArray = []
+
+function writeCards(array, event){
+  for (let i = 0; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+  }
+  return newArray;
+
+}
+
+
+function countdown(integer){
+  while (integer >= 0){
+    console.log(integer--);
+  }
+}
