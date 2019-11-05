87dc036b0606d071cfe5c897a1d7d1ac3fc0f340
John Blank <veloblank@gmail.com>
2019-11-05 01:42:44 -0500

    Refactored. Removed hard-coding text

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const newArray = []
-
-function writeCards(array, event){
-  for (let i = 0; i < array.length; i++){
-    newArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
-  }
-  return newArray;
-
+function writeCards(array, event) {
+	let newArray = [];
+	for (let i = 0; i < array.length; i++) {
+		newArray.push(
+			`Thank you, ${array[i]}, for the wonderful ${event} gift!`
+		);
+	}
+	return newArray;
 }
 
-
-function countdown(integer){
-  while (integer >= 0){
-    console.log(integer--);
-  }
+function countdown(integer) {
+	while (integer >= 0) {
+		console.log(integer--);
+	}
 }
